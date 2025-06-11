9ce7eeec09103a4b0e6981edb9d055bc4bcfc756
Joe Pires <pires.joe.a@gmail.com>
2025-06-11 00:22:18 -0400

    finish endpoint to get console, basics for post done

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,28 @@
 var express = require('express');
-const { connection } = require('../app');
+const mysql = require('mysql');
+const {DB_PASSWORD} = require('../constants.js');
 var router = express.Router();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: DB_PASSWORD,
+  database: 'video_game_collection'
+});
 
 // CONSOLES
 
 // Get Console Information
 router.get('/consoles/:id', async (req, res) => {
+    if (req.params.id == null) {
+        return res.status(400).json({ message: "Need To Include Console ID" });
+    }
     try {
         const id = parseInt(req.params.id);
-        const consoleVal = connection.query("SELECT * FROM Console WHERE id=$1", [id]);
-        res.json(consoleVal);
+        await connection.query(`SELECT * FROM Console WHERE id=${id}`, function (error, results, fields)  {
+            if (error) throw error;
+            return res.status(200).json(results);
+        });
     } catch (error) {
         console.log(error);
     }
@@ -18,6 +31,64 @@ router.get('/consoles/:id', async (req, res) => {
 
 // Create A New Console
 router.post('/consoles', async (req, res) => {
+    const bodyVal = req.body;
+    console.log(bodyVal);
+    console.log(bodyVal.name);
+    if (bodyVal.name == null) {
+        return res.status(400).json({ message: "Console Needs To Have A Name" });
+    }
+    /*
+    if (bodyVal.name.length == 0) {
+        return res.status(400).json({ message: "Console Name Cannot Be Empty String" });
+    }
+        */
+    if (bodyVal.console_type == null) {
+        return res.status(400).json({ message: "console_type Must Be Defined" });
+    }
+    if (bodyVal.region == null) {
+        return res.status(400).json({ message: "region Must Be Defined" });
+    }
+    if (bodyVal.product_condition == null) {
+        return res.status(400).json({ message: "product_condition Must Be Defined" });
+    }
+    if (bodyVal.has_packaging == null) {
+        return res.status(400).json({ message: "has_packaging Must Be Defined" });
+    }
+    if (bodyVal.is_duplicate == null) {
+        return res.status(400).json({ message: "is_duplicate Must Be Defined" });
+    }
+    if (bodyVal.has_cables == null) {
+        return res.status(400).json({ message: "has_cables Must Be Defined" });
+    }
+    try {
+        const entry = {
+            name: bodyVal.name,
+            console_type: bodyVal.console_type,
+            model: bodyVal.model,
+            region: bodyVal.region,
+            release_date: bodyVal.release_date,
+            bought_date: bodyVal.bought_date,
+            company: bodyVal.company,
+            product_condition: bodyVal.product_condition,
+            has_packaging: bodyVal.has_packaging,
+            is_duplicate: bodyVal.is_duplicate,
+            has_cables: bodyVal.has_cables,
+            monetary_value: bodyVal.monetary_value,
+            notes: bodyVal.notes
+        };
+        
+        await connection.query(
+            "INSERT INTO Console SET ?", entry, function(error, results, fields) {
+                if (error) throw error;
+                return res.status(201).json({
+                    message: "Console Created",
+                    result: results
+                });
+            });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error);
+    }
     res.json(req.body);
 });
 
