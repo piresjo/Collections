d829a28234f7e184cd58a9288d19596245075a06
Joe Pires <pires.joe.a@gmail.com>
2025-07-10 23:20:03 -0400

    validate game entry and accessory entry

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,19 @@ import { DB_PASSWORD } from "../secrets.js";
 import fs from "fs";
 import csv from "fast-csv";
 import { fileURLToPath } from "url";
-import multer from "multer";
 import {
   DERIVE_REGION,
   DERIVE_CONSOLE_TYPE,
   VALIDATE_CONSOLE_ENTRY_JSON,
   DERIVE_PRODUCT_CONDITION,
+  VALIDATE_GAME_ENTRY_JSON,
+  ConsolesAndIds,
+  VALIDATE_ACCESSORY_ENTRY_JSON,
+  DERIVE_ACCESSORY_TYPE,
 } from "../constants.js";
 var router = express.Router();
-const upload = multer({ dest: "public/csv/" });
 
 const __filename = fileURLToPath(import.meta.url);
-console.log(__filename);
 const __dirname = `C:\\Users\\Pires\\OneDrive\\Documents\\GitHub\\CollectionsDB\\public\\csv\\`;
 
 const connection = mysql.createConnection({
@@ -140,11 +141,10 @@ router.get("/accessories/:id", async (req, res) => {
 
 // Bulk Entry - Console
 router.get("/bulk_entry/consoles", async (req, res) => {
-  res.render("bulkentry.ejs");
+  res.render("bulkentry.ejs", { object: "Console" });
 });
 
 router.post("/bulk_entry/consoles", async (req, res) => {
-  // When a file has been uploaded
   if (req.files && Object.keys(req.files).length !== 0) {
     const uploadedFile = req.files.uploadFile;
     const uploadPath = __dirname + uploadedFile.name;
@@ -185,7 +185,7 @@ router.post("/bulk_entry/consoles", async (req, res) => {
               monetary_value:
                 console["Monetary Value"] === ""
                   ? null
-                  : parseFloat(console["Monetary Value"]),
+                  : parseFloat(console["Monetary Value"].trim().slice(1)),
               notes: console.Notes,
             };
 
@@ -219,29 +219,167 @@ router.post("/bulk_entry/consoles", async (req, res) => {
 });
 
 // Bulk Entry - Games
-router.post("/bulk_entry/games", upload.single("file"), async (req, res) => {
-  const title = req.body.title;
-  const file = req.file;
+router.get("/bulk_entry/games", async (req, res) => {
+  res.render("bulkentry.ejs", { object: "Game" });
+});
 
-  console.log(title);
-  console.log(file);
+router.post("/bulk_entry/games", async (req, res) => {
+  if (req.files && Object.keys(req.files).length !== 0) {
+    const uploadedFile = req.files.uploadFile;
+    const uploadPath = __dirname + uploadedFile.name;
+    var gameList = [];
 
-  res.sendStatus(200);
+    fs.createReadStream(uploadPath)
+      .pipe(csv.parse({ headers: true }))
+      .on("error", (error) => console.error(error))
+      .on("data", (row) => {
+        gameList.push(row);
+      })
+      .on("end", (rowCount) => {
+        console.log(`Parsed ${rowCount} rows`);
+        var resultsList = [];
+        gameList.forEach((game) => {
+          try {
+            const entry = {
+              has_game: game["Has Game"].toLowerCase() === "yes",
+              is_duplicate: game["Is Duplicate"].toLowerCase() === "yes",
+              has_manual: game["Has Manual"].toLowerCase() === "yes",
+              has_box: game["Has Box"].toLowerCase() === "yes",
+              monetary_value:
+                game["Monetary Value"] === ""
+                  ? null
+                  : parseFloat(game["Monetary Value"].trim().slice(1)),
+              notes: game.Notes,
+              console_id: ConsolesAndIds[game.Console.toLowerCase()],
+              name: game.Name,
+              edition: game.Edition === "" ? null : game.Edition,
+              release_date:
+                game["Release Date"] === ""
+                  ? null
+                  : new Date(game["Release Date"]).toISOString().split("T")[0],
+              bought_date:
+                game["Bought Date"] === ""
+                  ? null
+                  : new Date(game["Bought Date"]).toISOString().split("T")[0],
+              region: DERIVE_REGION(game.Region.toUpperCase()),
+              developer: game["Developer"] === "" ? null : game["Developer"],
+              publisher: game["Publisher"] === "" ? null : game["Publisher"],
+              digital: game["Digital"].toLowerCase() === "yes",
+              product_condition: DERIVE_PRODUCT_CONDITION(
+                game["Product Condition"].toLowerCase(),
+              ),
+            };
+            const errorVal = VALIDATE_GAME_ENTRY_JSON(entry);
+
+            if (errorVal != null) {
+              return res.status(400).json(errorVal);
+            }
+
+            connection.query(
+              "INSERT INTO Game SET ?",
+              entry,
+              function (error, results) {
+                if (error) throw error;
+                resultsList.push(results);
+              },
+            );
+          } catch (error) {
+            console.log(error);
+            return res.render("error.ejs", { error: error });
+          }
+        });
+        return res.render("created.ejs", {
+          object: "Game",
+          rowsAdded: rowCount,
+        });
+      });
+  } else {
+    res.send("No file uploaded !!");
+  }
 });
 
 // Bulk Entry - Accessories
-router.post(
-  "/bulk_entry/accessories",
-  upload.single("file"),
-  async (req, res) => {
-    const title = req.body.title;
-    const file = req.file;
+router.get("/bulk_entry/accessories", async (req, res) => {
+  res.render("bulkentry.ejs", { object: "Accessory" });
+});
 
-    console.log(title);
-    console.log(file);
+router.post("/bulk_entry/accessories", async (req, res) => {
+  if (req.files && Object.keys(req.files).length !== 0) {
+    const uploadedFile = req.files.uploadFile;
+    const uploadPath = __dirname + uploadedFile.name;
+    var accessoryList = [];
 
-    res.sendStatus(200);
-  },
-);
+    fs.createReadStream(uploadPath)
+      .pipe(csv.parse({ headers: true }))
+      .on("error", (error) => console.error(error))
+      .on("data", (row) => {
+        accessoryList.push(row);
+      })
+      .on("end", (rowCount) => {
+        console.log(`Parsed ${rowCount} rows`);
+
+        var resultsList = [];
+
+        accessoryList.forEach((accessory) => {
+          try {
+            const entry = {
+              console_id: ConsolesAndIds[accessory.Console.toLowerCase()],
+              name: accessory.Name,
+              model: accessory.Model,
+              accessory_type: DERIVE_ACCESSORY_TYPE(
+                accessory["Accessory Type"].toLowerCase(),
+              ),
+              release_date:
+                accessory["Release Date"] === ""
+                  ? null
+                  : new Date(accessory["Release Date"])
+                      .toISOString()
+                      .split("T")[0],
+              bought_date:
+                accessory["Bought Date"] === ""
+                  ? null
+                  : new Date(accessory["Bought Date"])
+                      .toISOString()
+                      .split("T")[0],
+              company: accessory.Company === "" ? null : accessory.Company,
+              product_condition: DERIVE_PRODUCT_CONDITION(
+                accessory["Product Condition"].toLowerCase(),
+              ),
+              has_packaging: accessory["Has Packaging"].toLowerCase() === "yes",
+              monetary_value:
+                accessory["Monetary Value"] === ""
+                  ? null
+                  : parseFloat(accessory["Monetary Value"].trim().slice(1)),
+              notes: accessory.Notes,
+            };
+
+            const errorVal = VALIDATE_ACCESSORY_ENTRY_JSON(entry);
+
+            if (errorVal != null) {
+              return res.status(400).json(errorVal);
+            }
+
+            connection.query(
+              "INSERT INTO Accessory SET ?",
+              entry,
+              function (error, results) {
+                if (error) throw error;
+                resultsList.push(results);
+              },
+            );
+          } catch (error) {
+            console.log(error);
+            return res.render("error.ejs", { error: error });
+          }
+        });
+        return res.render("created.ejs", {
+          object: "Accessory",
+          rowsAdded: rowCount,
+        });
+      });
+  } else {
+    res.send("No file uploaded !!");
+  }
+});
 
 export default router;
